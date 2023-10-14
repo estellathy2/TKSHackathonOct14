bac2d83c882b3cae30b7dde15e6d63c36947992b
realBarry123 <barryyu28@gmail.com>
2023-10-14 21:20:03 +0900

    Write prompt template

--- TKSHackathonOct14/chain.py
+++ TKSHackathonOct14/chain.py
@@ -13,41 +13,22 @@ warnings.filterwarnings('ignore')
 
 llm = ChatOpenAI(temperature = 0.9, model = llm_model)
 
-
-prompt_1 = ChatPromptTemplate.from_template("Make sure this message does not contain anything similar with It’s your lucky day!")
-chain_1 = LLMChain(llm = llm, prompt = prompt_1, output_key = "lucky_day")
-
-
-prompt_2 = ChatPromptTemplate.from_template("Make sure this message is not talking about a prize")
-chain_2 = LLMChain(llm = llm, prompt = prompt_2, output_key = "prize")
-
-
-prompt_3 = ChatPromptTemplate.from_template("Make sure this message is not telling me to visit a link")
-chain_3 = LLMChain(llm = llm, prompt = prompt_3, output_key = "visit_link")
-
-
-prompt_4 = ChatPromptTemplate.from_template("Make sure this message does not say something urgent is happening")
-chain_4 = LLMChain(llm = llm, prompt = prompt_4, output_key = "urgent")
-
-
-prompt_5 = ChatPromptTemplate.from_template("Make sure the person texting me does not have a number starting with 604")
-chain_5 = LLMChain(llm = llm, prompt = prompt_5, output_key = "604")
-
-
-prompt_6 = ChatPromptTemplate.from_template("Make sure the person texting me does not have a number starting with 778")
-chain_6 = LLMChain(llm = llm, prompt = prompt_6, output_key = "778")
-
-
-prompt_7 = ChatPromptTemplate.from_template("Make sure the person texting me does not have a number starting with 236")
-chain_7 = LLMChain(llm = llm, prompt = prompt_7, output_key = "236")
-
-
-prompt_8 = ChatPromptTemplate.from_template("Make sure the person texting me does not have a number starting with 672")
-chain_8 = LLMChain(llm = llm, prompt = prompt_8, output_key = "672")
-
-
-prompt_9 = ChatPromptTemplate.from_template("Make sure the message is not asking for a payment")
-chain_9 = LLMChain(llm = llm, prompt = prompt_9, output_key = "payment")
+prompt_template = ChatPromptTemplate.from_template(
+    """\
+    This is a prompt engineered for a scam detection software.
+    For the text message: "{message}", give a score for the confidence for the validity of each of the following 3 items:
+    1. Using this text message, the sender intends to scam or decieve the recipient in some way for personal gain.
+    2. Using this text message, the sender intends to steal the recipient's money.
+    3. Using this text message, the sender intends to steal the recipient's identity.
+    The confidence score should be a number from 0 (least confident) to 1 (most confident), to two decimal places. Refrain from using absolute confidence scores as they can be unrealistic.
+    Format the output as JSON with the following keys:
+    "scam"
+    "money"
+    "info"
+    
+    Output the JSON only. Do not provide explanation.
+    """
+)
 
 
 def predict(input, history=[]):
